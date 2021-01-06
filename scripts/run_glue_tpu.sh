3c3ac74b332da81e83838e1c43abbc433565b2b8
Ameet Deshpande <ameetsd97@gmail.com>
2021-01-06 04:19:48 -0500

    Updated glue script

--- scripts/run_glue_tpu.sh
+++ scripts/run_glue_tpu.sh
@@ -1,7 +1,7 @@
 export TASK_NAME=mnli
 export RUN_NO=_1
 
-python ../transformers/examples/xla_spawn.py --num_cores 1 ../transformers/examples/run_glue.py \
+python ../transformers/examples/xla_spawn.py --num_cores 1 ../transformers/examples/text-classification/run_glue.py \
   --model_name_or_path ../../../bucket/model_outputs/wikitext/mono_english/ \
   --task_name $TASK_NAME \
   --do_train \
@@ -10,4 +10,7 @@ python ../transformers/examples/xla_spawn.py --num_cores 1 ../transformers/examp
   --per_device_train_batch_size 32 \
   --learning_rate 2e-5 \
   --num_train_epochs 3 \
-  --output_dir ../../../bucket/model_outputs/glue/$TASK_NAME$RUN_NO/
+  --save_steps 5000 \
+  --overwrite_output_dir \
+  --output_dir ../../../bucket/model_outputs/glue/$TASK_NAME$RUN_NO/ \
+  --run_name glue_$TASK_NAME
